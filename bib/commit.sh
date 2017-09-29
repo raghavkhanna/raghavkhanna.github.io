7858fd760b03aa98627e89a0c518932065c8ae30
Raghav Khanna <raghavkhanna2007@gmail.com>
2017-09-29 12:38:09 +0200

    test update script

--- bib/commit.sh
+++ bib/commit.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+#Usage: ./commit.sh "commit message"
+# adding files needs to be done separately
 cd ../raghavkhanna.github.io
+git commit -am $1
+cd ..
+git commit -am $1
+git push --recurse-submodules=on-demand
