35a99caca755e7f0d8b76d48cbfaac25deb51c97
Raghav Khanna <raghavkhanna2007@gmail.com>
2017-10-02 17:42:04 +0200

    update ICRA 2017 paper full text link

--- bib/commit.sh
+++ bib/commit.sh
@@ -4,10 +4,9 @@
 # adding files needs to be done separately
 #unset GIT_DIR
 cd ../../raghavkhanna.github.io
-git status
-#git commit -am $1
+git add .
+git commit -m "$1"
 cd ..
-git status
-#git status
-#git commit -am $1
-#git push --recurse-submodules=on-demand
+git add .
+git commit -m "$1"
+git push --recurse-submodules=on-demand
